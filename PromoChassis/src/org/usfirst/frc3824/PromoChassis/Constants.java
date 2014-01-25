70055221e988a5333e6642e12707e60f08080d8a
John Young <jyoung1@knology.net>
2014-01-25 14:21:15 -0500

    Project was duplicated in folder fixed problem.

---
+++ PromoChassis/src/org/usfirst/frc3824/PromoChassis/Constants.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc3824.PromoChassis;
+
+/**
+ * Put any important constants here.
+ */
+public class Constants {
+    /* Contants which might require tuning */
+    public final static double DrivetrainAngleGyroControllerP = .1;
+    public final static double DrivetrainAngleGyroControllerI = .01;
+    public final static double DrivetrainAngleGyroControllerD = .01;
+}
